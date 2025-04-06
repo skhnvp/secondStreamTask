7c7c2d0165d689c2a88c7ffc77765ac57042e93c
Pavel <skhnvp@gmail.com>
2025-04-06 16:12:01 +0300

    init

---
+++ src/main/java/ru/stepup/Main.java
@@ -0,0 +1,35 @@
+package ru.stepup;
+
+/*
+Перед вами готовый к использованию метод printList(ArrayList<Integer>),
+который  все элементы списка по порядку выводит в консоль.
+
+Сейчас метод реализован с использованием оператора for.
+
+Задача: переписать реализацию метода printList(ArrayList<Integer>),
+используя метод списка forEach(), принимающий лямбда-выражение.
+Логика работы метода остается той же.
+
+Подсказка: нужно использовать лямбда-выражение, передавая его в метод forEach().
+
+public static void printList(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        printList(Arrays.asList(1,2,3,4,5));
+    }
+
+    public static void printList(List<Integer> list) {
+        list.forEach(System.out::println);
+    }
+}
